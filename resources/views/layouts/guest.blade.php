e1570f2ea6c888c615070cd48d7bc869d5b43069
Prosper-Tshumaa <tshumaprosper11@gmail.com>
2023-09-11 19:16:24 +0400

    Added footer, Hero , logo and made changes to code

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -47,11 +47,7 @@
     </main>
 
     <!-- Footer -->
-    <footer class="bg-white border-t border-gray-200">
-      <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <span class="text-gray-500">&copy; {{ date('Y') }} {{ config('app.name') }}</span>
-      </div>
-    </footer>
+    @include('components.footer')
   </div>
 </body>
 
